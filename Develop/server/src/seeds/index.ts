363f774319daf89cbc82509848abaffb20ccce84
Mitch <mitchjoelklein@hotmail.com>
2024-12-09 13:30:06 +1100

    Added error handler to index.ts

--- Develop/server/src/seeds/index.ts
+++ Develop/server/src/seeds/index.ts
@@ -5,9 +5,14 @@ import { sequelize } from "../models/index.js";
 
 const seedAll = async (): Promise<void> => {
   try {
-    // @ts-ignore
-    await sequelize.sync({ force: true });
-    console.log("\n----- DATABASE SYNCED -----\n");
+    try {
+      // @ts-ignore
+      await sequelize.sync({ force: true });
+      console.log("\n----- DATABASE SYNCED -----\n");
+    } catch (error) {
+      console.error("Error syncing database:", error);
+      process.exit(1);
+    }
 
     await seedUsers();
     console.log("\n----- USERS SEEDED -----\n");
